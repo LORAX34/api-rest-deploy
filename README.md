87955ef17c7095342d91061323a3f25f64affc1f
Lorax_34 <ivanalejandroperezjimenez3@gmail.com>
2025-01-04 21:43:13 -0500

    actu readmi

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# api-rest
+# Api 
+
+Estamos en el proceso de desarrollo de la api
+ya se implemento en MVC pero hay que hacerle mas mejoras
--- controllers/personas.js
+++ controllers/personas.js
@@ -2,22 +2,18 @@ import { PersonaModel } from '../models/personaje.js'
 
 export class PersonaController {
     static async getAll(req,res){
-         
         const {house} = req.query
         const hp =  await PersonaModel.getAll({house})
         res.json(hp)
-    
     }
 
     static async getById (req,res){
-
         const {id} = req.params
         const hp = await PersonaModel.getById({id})
         if(hp) return res.json(hp)
             res.status(404).json({
                 message: "Personaje not Found"
             })
-    
     }
 
     static async deleteById(req,res){
